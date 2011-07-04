d3f3d05437e83baca6cf0ab9b1ebfe95f4cf9313
David Thomas <dave@davespace.co.uk>
2011-07-04 17:05:19 +0100

    Character keys/values.

---
+++ char-kv.c
@@ -0,0 +1,40 @@
+/* char-kv.c -- functions for items which use chars */
+
+#include "types.h"
+
+#include <ctype.h>
+#include <stdio.h>
+
+#include "char-kv.h"
+
+size_t charkv_len(const void *key_)
+{
+  NOT_USED(key_);
+
+  return sizeof(char);
+}
+
+int charkv_compare(const void *a_, const void *b_)
+{
+  int a = *((const char *) a_);
+  int b = *((const char *) b_);
+
+  if (a > b) return 1;
+  else if (a < b) return -1;
+  else return 0;
+}
+
+const char *charkv_fmt(const void *kv)
+{
+  static char str[9]; /* to fit '<-128>' + NUL */
+  char        c;
+  
+  c = *((const char *) kv);
+  
+  if (isprint(c))
+    sprintf(str, "'%c'", c);
+  else
+    sprintf(str, "'<%d>'", c);
+
+  return str;
+}
